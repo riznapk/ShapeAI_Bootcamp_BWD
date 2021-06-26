f0cc07fc5dc802a6955a3a8009d9259b47e39c4d
riznapk <32645933+riznapk@users.noreply.github.com>
2021-06-26 20:06:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Info from "./Info";
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <Footer/>
+      <Info/>
+      <Info/>
+    </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return(
+    <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p>A 7 day bootcamp conducted by Shape AI. We covered everything from scratch including HTML, CSS, Javascript, ReactJS.</p>
+    </div>
+    );
+}
+
+export default Info;
